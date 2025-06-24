fc60e8906b5189be4d7646a2f637023a9223de8d
abelriu <abelriu1@gmail.com>
2025-06-24 17:03:46 +0200

    primer commit

--- index.js
+++ index.js
@@ -81,4 +81,5 @@ app.post('/search', async (req, res) => {
 }
 });
 
-app.listen(3000, () => console.log('Servidor en puerto 3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor en puerto ${PORT}`));
